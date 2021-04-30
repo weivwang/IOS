0ed94b942fea150331504ae29c9ea0ea9c3ec25d
weivwang <54494072+weivwang@users.noreply.github.com>
2021-04-30 10:12:11 +0800

    Add files via upload

---
+++ 03-/03-001/03-001/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  03-001
+//
+//  Created by 王威卫 on 2021/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var labX: UILabel!
+    var previous:Int = 0
+    var opt:String = ""
+    var isAppending = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func digiTapped(_ sender: UIButton!) {
+        
+    }
+    
+}
+
